1b8fadbe30260377c1cadb22462e949c268b6c0c
DileepKumar <141676654+MullaguraDileepKumar@users.noreply.github.com>
2024-11-09 04:53:49 +0530

    Add files via upload
    bin folder missing in UserManagement

---
+++ Core/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+using UserManagement.Core.Dtos.Auth;
+using UserManagement.Core.Dtos.General;
+
+namespace UserManagement.Core.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<GeneralServiceResponseDto> SeedRolesAsync();
+        Task<GeneralServiceResponseDto> RegisterAsync(RegisterDto registerDto);
+        Task<LoginServiceResponseDto> LoginAsync(LoginDto loginDto);
+        Task<GeneralServiceResponseDto> UpdateRoleAsync(ClaimsPrincipal User,UpdateRoleDto updateRoleDto);
+
+    }
+}
